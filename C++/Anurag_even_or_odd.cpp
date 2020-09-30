cbebdb9b5b2b0cc53af7152d59d7c7fb31a216af
Anurag Gupta <aguptaking@gmail.com>
2020-09-30 16:08:53 +0530

    Even_or_Odd

---
+++ C++/Anurag_even_or_odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << n << " is a even number." << endl;
+    }
+    else
+    {
+        cout << n << " is a odd number." << endl;
+    }
+}
