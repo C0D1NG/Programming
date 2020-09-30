cbb3efa0b5239ac726fdc980895ec46d9d137528
Varun Vaibhav Jha <60656060+varunvjha@users.noreply.github.com>
2020-09-30 10:06:35 +0530

    Add files via upload

---
+++ Java/DigitSum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class DigitSum {
+	static int sum(int n) {
+		int s = 0;
+		while(n>0) {
+			s = s+n%10;
+			n=n/10;
+		}
+		return s;
+	}
+	
+	public static void main(String []args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = in.nextInt();
+		int s = sum(n);
+		System.out.println("Sum of digits = "+s);
+	}
+}
---
+++ Java/ReverseNumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Varun Vaibhav Jha
+ *
+ */
+public class ReverseNumber {
+
+	/**
+	 * @param args
+	 */
+	
+	static int reverse(int n) {
+		int r = 0, rev = 0;
+		while(n>0) {
+			r = n%10;
+			rev = rev*10+r;
+			n=n/10;
+		}
+		return rev;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = in.nextInt();
+		int r = reverse(n);
+		System.out.println("Reverse of " +n + " = "+r);
+
+	}
+
+}
