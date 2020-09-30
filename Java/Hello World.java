83233348e058d2baabf5a440b768d0b9354fa732
Sourakanti Mandal <52707495+Sonu64@users.noreply.github.com>
2020-09-30 18:55:29 +0530

    Improved Understandability via comments.

--- Java/Hello World.java
+++ Java/Hello World.java
@@ -1,7 +1,10 @@
 import java.io.*;
 
 class Program {
+	/* main function is the function that will run before anything else*/
 	public static void main (String[] args) {
+		// System.out.println() is used to print a string to console
+		// System.out.print() will not include a new Line at the end.
 		System.out.println("Hello, World!");
 	}
 }
