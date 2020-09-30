00d6b05fe9c46dc350137258b1ef3b4e45bb2b6d
SHYAM PRAVEEN SINGH <shyampraveensingh@gmail.com>
2020-10-01 00:27:30 +0530

    Shyam_sum.c

---
+++ C/Shyam_Sum.c
@@ -0,0 +1,23 @@
+//Sum of digits of a number
+
+#include <stdio.h>
+int main()
+{
+   int n, t, sum = 0, remainder;
+
+   printf("Enter an integer\n");
+   scanf("%d", &n);
+
+   t = n;
+
+   while (t != 0)
+   {
+      remainder = t % 10;
+      sum       = sum + remainder;
+      t         = t / 10;
+   }
+
+   printf("Sum of digits of %d = %d\n", n, sum);
+
+   return 0;
+}
