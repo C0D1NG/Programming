14a9a752f906266a8b9c173164d9ddf89b1e2ed3
Maitryee-Choudhary <67001353+Maitryee-Choudhary@users.noreply.github.com>
2020-10-01 00:44:28 +0530

    Create Maitryee-Choudhary_Leap Year or not.cpp

---
+++ Maitryee-Choudhary_Leap Year or not.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+using namespace std;  
+  
+bool checkYear(int year)  
+{  
+    // If a year is multiple of 400,  
+    // then it is a leap year  
+    if (year % 400 == 0)  
+        return true;  
+  
+    // Else If a year is muliplt of 100,  
+    // then it is not a leap year  
+    if (year % 100 == 0)  
+        return false;  
+  
+    // Else If a year is muliplt of 4,  
+    // then it is a leap year  
+    if (year % 4 == 0)  
+        return true;  
+    return false;  
+}  
+  
+
+int main()  
+{  
+    int year;  
+    cin>>year;
+    checkYear(year) ? cout << "Leap Year":  
+                      cout << "Not a Leap Year";  
+    return 0;  
+}  
