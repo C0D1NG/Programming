a88f088b910aaff7382099bea9ab552892283b9d
Bhubhanshu Gurjar <31334290+bhubhanshu@users.noreply.github.com>
2020-10-01 10:35:30 +0530

    Update Bhubhanshu_hcf.php

--- PHP/Bhubhanshu_hcf.php
+++ PHP/Bhubhanshu_hcf.php
@@ -5,11 +5,5 @@
         return $b==0 ? $a : hcf($b, $a % $b);
     }
 
-    if($a < $b){
-        $temp = $a;
-        $a = $b;
-        $b = $temp;
-    }
-
     echo "Highest Common Factor of " . $a ." and ". $b ." is ". hcf($a, $b);
 ?>
