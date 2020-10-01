0bec0f1ae5d128d03a08fa65feb1463343915e01
gourav <you@example.com>
2020-10-02 00:39:12 +0530

    add binary to decimal in c

---
+++ C/Gourav_binary_into_decimal.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+// Function to convert binary to decimal
+int binaryToDecimal(int n)
+{
+    int num = n;
+    int dec_value = 0;
+    int base = 1;
+    int temp = num;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+        dec_value += last_digit * base;
+        base = base * 2;
+    }
+    return dec_value;
+}
+int main()
+{
+    int num = 101;
+   printf("%d",binaryToDecimal(num));
+}
