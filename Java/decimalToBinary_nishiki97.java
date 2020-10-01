cf8bb9783156f697039e26206d735975b0d3cc2a
Nishiki97 <nishikiyapa1997@gmail.com>
2020-10-01 14:12:05 +0530

    added decimalToBinary_nishiki97 file

--- Java/decimalToBinary_nishiki97.java
+++ Java/decimalToBinary_nishiki97.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class decimalToBinary_nishiki97 {
     public static void main(String[] args) {
         
+        /**
+         * @toBinary(number) - calling the inclass function in the main 
+         */
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter decimal number -> ");
@@ -16,6 +20,7 @@ public class decimalToBinary_nishiki97 {
         
     }
 
+    //creating a method to convert binary number into decimal number
     public static void toBinary(int decimalNum){    
         
         int binary[] = new int[40];    
