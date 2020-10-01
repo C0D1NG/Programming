d6e80f0ff0facee461cf8938335ed0564d021af2
Adiv Harjadinata <adiv.dinata@gmail.com>
2020-10-01 07:12:49 +0700

    Added decimaltoBinary C++

---
+++ C++/adivdn_decimaltoBinary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout<< "Enter your decimal number : ";
+    cin >> n;
+    cout<<endl;
+    int binary[n];
+
+    int i = 0;
+    cout<< "Decimal to Binary From "<< n <<" = "; 
+    while (n > 0){
+        binary[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    
+    for(int j = i -1; j >= 0; j--){
+        cout<<binary[j];
+    }
+    cout<<endl;
+
+    return 0;
+}
