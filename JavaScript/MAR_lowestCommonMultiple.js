f8365dfd5443500ceecffeca79af26c8b86774ad
muhdarifrawi <muhdarifrawi@gmail.com>
2020-10-04 16:39:25 +0800

    added programs ability to further expand scope of multiples.

--- JavaScript/MAR_lowestCommonMultiple.js
+++ JavaScript/MAR_lowestCommonMultiple.js
@@ -5,9 +5,14 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+let firstNumber = 0;
+let secondNumber = 0;
+
 rl.question("Please enter first number >>> ", function(n1) {
     rl.question("Please enter second number >>> ", function(n2) {
-        lowestMultiple(n1,n2);
+        firstNumber = n1;
+        secondNumber = n2;
+        lowestMultiple(firstNumber,secondNumber);
         if(commonNumbers.length == 0){
             console.log("Sorry no common lowest multiple found.\n (TnT)")
         }
@@ -26,18 +31,36 @@ rl.on("close", function() {
 let n1Multiples = [];
 let n2Multiples = [];
 let commonNumbers = [];
+let attempt = 0;
+let counter = 0;
+let min = 0;
+let max = 10;
 
 function lowestMultiple(n1,n2){
-    for(i=0;i<10;i++){
-        n1current = n1 * i;
-        n1Multiples.push(n1current);
-        n2current = n2 * i;
-        n2Multiples.push(n2current);
+    if(counter != attempt){
+        min += 10;
+        max += 10;
+        for(i=min;i<max;i++){
+            n1current = n1 * i;
+            n1Multiples.push(n1current);
+            n2current = n2 * i;
+            n2Multiples.push(n2current);
+        }
+        counter+=1;
+    }
+    else{
+        for(i=min;i<max;i++){
+            n1current = n1 * i;
+            n1Multiples.push(n1current);
+            n2current = n2 * i;
+            n2Multiples.push(n2current);
+        }
     }
     check();
 }
 
 function check(){
+    attempt += 1;
     for(i in n1Multiples){
         for(j in n2Multiples){
             if(n1Multiples[i]==n2Multiples[j]){
@@ -45,5 +68,9 @@ function check(){
                 break;
             }
         }
+    }
+    if(commonNumbers.length<=1){
+        commonNumbers=[];
+        lowestMultiple(firstNumber,secondNumber);
     }
 }
