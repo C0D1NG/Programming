31cbcb5be69d6c6ea6dd549fdee02e56f79d05f8
funwaa <56044926+funwaa@users.noreply.github.com>
2020-10-05 13:14:35 +0200

    Update find_second_largest_number.sh
    Can also find nth smallest/largest number

--- Bash/find_second_largest_number.sh
+++ Bash/find_second_largest_number.sh
@@ -3,4 +3,14 @@
 # list is loaded from numbers.txt in the directory the script is run in, each number being in its own line
 
 listfile = "numbers.txt"
-cat $listfile | sort -n | tail -n -2 | head -n 1
+type = "largest" # this can be largest or smallest
+nth = "2" # find the NTH largest or smallest number
+
+if [[ "$type" == "largest" ]]; then
+  cat $listfile | sort -n | tail -n -${nth} | head -n 1
+elif [[ "$type" == "smallest" ]]; then
+  cat $listfile | sort -n | head -n ${nth} | tail -n 1
+else
+  echo "Unknown type."
+  exit 1
+fi
