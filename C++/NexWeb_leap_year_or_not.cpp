be40cfa3b5cbfda5a2aeadb3a9ed16eabbe41d52
Nexus Web Development <github@nexwebsites.com>
2020-10-06 05:24:08 -0400

    Update NexWeb_leap_year_or_not.cpp

--- C++/NexWeb_leap_year_or_not.cpp
+++ C++/NexWeb_leap_year_or_not.cpp
@@ -6,7 +6,7 @@ int main()
     std::cout << "Enter a year: ";
     std::cin >> year;
     if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-        std::cout << year << " is a leap year";
+        std::cout << year << " is a leap year\n";
     else
         std::cout << year << " is not a leap year\n";
     return 0;
