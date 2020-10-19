cac1a2dd6e5fda9a1a862108c5d949bba83a34be
Samarth Dad <samarthdaad2800@gmail.com>
2020-10-19 23:35:57 +0530

    Hex to ASCII

---
+++ Python/samarthdad_hex_to_ascii.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python 
+# Python3 program to illustrate 
+# hex() function 
+  
+print("The hexadecimal form of 23 is"
+                            + hex(23)) 
+                              
+print("The hexadecimal form of the "
+      "ascii value is 'a' is " + hex(ord('a'))) 
+        
+print("The hexadecimal form of 3.9 is "
+                        + float.hex(3.9)) 
+ 
