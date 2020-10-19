8ed5b3f4fb7c97c0ca8998137f3c3f0aea65469b
Aniket Ghosh <ghoshaniketkumar7@gmail.com>
2020-10-20 03:38:57 +0530

    Added ak7550_DecimalToBinary that code prints a binary String for a given decimal number.

---
+++ Java/ak7550_DecimalToBinary.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ak7550_DecimalToBinary {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.toBinaryString(sc.nextInt()));
+        sc.close();
+    }
+}
