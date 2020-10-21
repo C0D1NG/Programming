6770b6a6550f3af60c9c5c0787b73fb40e066720
Poojasingh5 <59305317+Poojasingh5@users.noreply.github.com>
2020-10-21 06:49:33 +0530

    Create Poojasingh5-factorial.cpp

---
+++ Poojasingh5-factorial.cpp
@@ -0,0 +1,28 @@
+// ISSUE -#13
+//PROGRAM TO FIND FACTORIAL OF A GIVEN NUMBER
+
+#include <iostream>
+using namespace std;
+typedef long long int ll;
+
+  ll factorial (ll n)
+ {
+     if (n == 0) 
+        return 1; 
+    return n * factorial(n - 1);
+ }
+
+ int main()
+ {
+     ios_base::sync_with_stdio(false);
+	 cin.tie(NULL);
+	 cout.tie(NULL);
+    ll n;
+    cout<<"Enter Number="<<endl;
+    cin>>n;
+    ll ans=factorial(n);
+    cout<<"Factorial of "<<n<<" is "<<ans<<endl;
+    
+     
+    return 0;
+   }
