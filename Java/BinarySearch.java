4dd1fe64ccd5da66585cccb96158eb3b06bbb176
khageshwor <khageshwarjoshi03@gmail.com>
2020-11-17 20:33:44 +0545

    Code Formatted

--- Java/BinarySearch.java
+++ Java/BinarySearch.java
@@ -1,9 +1,10 @@
     class BinarySearch{  
+
         public static int BinarySearch(int arr[], int first, int last, int key){  
 
             if (last >= first){  
 
-                int mid = first + (last - first)/2;  
+                int mid = first + (last - first) / 2;  
 
                 if (arr[mid] == key){ 
 
@@ -13,11 +14,11 @@
 
                 if (arr[mid] > key){ 
 
-                return BinarySearch(arr, first, mid-1, key);    //search in left subarray  
+                return BinarySearch(arr, first, mid - 1, key);    //search in left subarray  
 
                 }else{ 
 
-                return BinarySearch(arr, mid+1, last, key);     //search in right subarray  
+                return BinarySearch(arr, mid + 1, last, key);     //search in right subarray  
 
                 }  
             }  
@@ -25,7 +26,7 @@
         }  
         public static void main(String args[]){  
 
-            int arr[] = {10,20,30,40,50};  
+            int arr[] = {10, 20, 30, 40, 50};  
 
             int key = 30;  
 
