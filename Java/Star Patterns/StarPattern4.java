418fcd39deb4215736a12bfc2625b892ebbf70bd
Vijay <viijay1999@gmail.com>
2023-10-05 19:57:44 +0530

    add star pattern java programs

---
+++ Java/Star Patterns/StarPattern4.java
@@ -0,0 +1,16 @@
+public class StarPattern4 {
+
+	public static void main(String[] args) {
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.print(" ");
+		}
+		for(int k=5;k>=i;k--) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+
+	}
+
+}
